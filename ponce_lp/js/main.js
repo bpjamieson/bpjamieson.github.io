48bcff9e4b290124e470fa795efaa67eb68f6ba4
Ben Jamieson <benjamieson09@gmail.com>
2017-05-19 09:14:17 -0400

    version 20

--- ponce_lp/js/main.js
+++ ponce_lp/js/main.js
@@ -1,4 +1,8 @@
-$(document).ready(function() {
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
 
 // $('h1').text('Hello!');
 
@@ -9,5 +13,18 @@ $('button').hover(function(){
     $('#mobile-arrow').toggleClass('moved');
 });
 
+if (distanceScrolled > 1600) {
+  $('.icon-img').addClass('animated');
+} else {
+  $('.icon-img').removeClass('animated');
+}
+
+
+
+// if (distanceScrolled > 1400) {
+//   $('.butterfly').addClass('animated');
+// } else {
+//   $('.butterfly').removeClass('animated');
+// }
 
 });
