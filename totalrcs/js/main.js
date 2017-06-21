aa3e6bc777b017af090882e2f52211db52119f1c
Ben Jamieson <benjamieson09@gmail.com>
2017-06-21 14:31:08 -0400

    v35

---
+++ totalrcs/js/main.js
@@ -0,0 +1,20 @@
+$(window).on('scroll', function () {
+	// Step 1: Google $(window).scrollTop();
+	var distanceScrolled = $(window).scrollTop();
+
+	console.log('The distance scrolled is: ' + distanceScrolled);
+
+
+	if (distanceScrolled > 825) {
+		$('.wheelbarrow, .mower').addClass('animate-icons');
+	}
+
+	if (distanceScrolled > 1025) {
+		$('.painter, .trashcan').addClass('animate-icons');
+	}
+
+	if (distanceScrolled > 1225) {
+		$('.powerwasher, .driveway').addClass('animate-icons');
+	}
+
+});
