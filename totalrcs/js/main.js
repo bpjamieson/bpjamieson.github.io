ae9755b6a2c3d74ce6a06fc188aa4ec0eb5ab4e0
Ben Jamieson <benjamieson09@gmail.com>
2017-06-23 13:36:25 -0400

    v67

--- totalrcs/js/main.js
+++ totalrcs/js/main.js
@@ -1,5 +1,14 @@
+
+
+$(function() {
+  $('.header-box').addClass('animate-header-box');
+
+});
+
+
+
 $(window).on('scroll', function () {
-	// Step 1: Google $(window).scrollTop();
+// Step 1: Google $(window).scrollTop();
 	var distanceScrolled = $(window).scrollTop();
 
 	console.log('The distance scrolled is: ' + distanceScrolled);
